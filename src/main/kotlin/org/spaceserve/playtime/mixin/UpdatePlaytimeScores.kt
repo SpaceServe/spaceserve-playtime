e4474d241ca88055a62597546a1a670f5f6d3832
SpaceClouds42 <spaceclouds42@gmail.com>
2021-07-22 14:02:31 -0400

    make updateScores mixin less brittle

--- src/main/kotlin/org/spaceserve/playtime/mixin/UpdatePlaytimeScores.kt
+++ src/main/kotlin/org/spaceserve/playtime/mixin/UpdatePlaytimeScores.kt
@@ -5,16 +5,22 @@ import org.spaceserve.playtime.Common
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.Slice
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 @Mixin(ServerPlayerEntity::class)
 abstract class UpdatePlaytimeScores {
     @Inject(
         method = ["playerTick"],
+        slice = [Slice(
+            from = At(
+                value = "FIELD",
+                target = "Lnet/minecraft/server/network/ServerPlayerEntity;lastLevelScore:I",
+            )
+        )],
         at = [At(
             value = "INVOKE",
             target = "Lnet/minecraft/server/network/ServerPlayerEntity;updateScores(Lnet/minecraft/scoreboard/ScoreboardCriterion;I)V",
-            ordinal = 5,
             shift = At.Shift.BY,
             by = 2,
         )]
